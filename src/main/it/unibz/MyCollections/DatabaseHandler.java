2738ede9034a9b3622d6e02ada2fba5b6f93522d
Claudio Spiess <claudiosv@users.noreply.github.com>
2017-05-14 17:49:25 +0200

    progress

--- src/main/it/unibz/MyCollections/DatabaseHandler.java
+++ src/main/it/unibz/MyCollections/DatabaseHandler.java
@@ -15,11 +15,20 @@ public class DatabaseHandler {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:test.db");
             Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS records (\n"
+            String sql = "CREATE TABLE IF NOT EXISTS entries (\n"
                     + "	id integer PRIMARY KEY,\n"
-                    + "	name text NOT NULL,\n"
-                    + "	capacity real\n"
+                    + "	userid integer NOT NULL,\n"
+                    + " FOREIGN KEY(userid) REFERENCES users(id),\n"
+                    + "	firstname TEXT,\n"
+                    + "	lastname TEXT,\n"
+                    + "	companyname TEXT,\n"
+                    + "	address TEXT,\n"
+                    + "	telephonenumber TEXT,\n"
+                    + "	email TEXT,\n"
+                    + "	picture BLOB,\n"
                     + ");";
+
+            /*first name, last name, company name, address, telephone number, e-mail address,*/
             stmt.execute(sql);
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
